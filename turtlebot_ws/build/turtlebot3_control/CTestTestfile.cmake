89297dfc97e79f96b1a0aee789baeb8578aaffa1
Ahmed Saber <67476932+ahmedsaber12348@users.noreply.github.com>
2024-09-11 17:56:10 +0300

    Delete turtlebot_ws directory

--- turtlebot_ws/build/turtlebot3_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahmedsaber/turtlebot_ws/src/turtlebot3_control
-# Build directory: /home/ahmedsaber/turtlebot_ws/build/turtlebot3_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
